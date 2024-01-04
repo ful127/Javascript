458089bccea1e712260abcfaf213fc1cdb9d157a
Abhishek sharma <132250580+ful127@users.noreply.github.com>
2024-01-04 10:06:19 +0000

    datatype conversion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 14373846;
+var accountEmail = "abhi476";
+let accountCity = "Jaipur";
+console.log(accountId);
+console.log(accountCity);
+console.table([accountId , accountEmail , accountCity]);
