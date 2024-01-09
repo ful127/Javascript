fc7a74a23f1c00617997ca7387b1125f45fd58bd
Abhishek sharma <132250580+ful127@users.noreply.github.com>
2024-01-09 08:55:21 +0000

    number and maths

---
+++ 01_basics/03_stackandheap.js
@@ -0,0 +1,14 @@
+var name1 = "Abhishek Sharma";
+var name2 = name1;
+name2 = "Dipanshu Sharma"
+console.log(name1);
+console.log(name2);
+let user1 ={
+    email: "abhi@123",
+    name: "abhi",
+    upi: "abhi"
+}
+user2 =  user1
+user2.email = "abhi@123456";
+console.log(user1.email)
+console.log(user2.email)
---
+++ 01_basics/04_number_and_maths.js
@@ -0,0 +1,16 @@
+var myNumber = 100;
+console.log(myNumber);
+var myNumber = new Number(200);
+console.log(myNumber);
+
+const balance = 400;
+console.log(balance.toString())
+console.log(balance.toFixed(3));
+
+const score = 1234.999999;
+console.log(score);
+console.log(score.toPrecision(19))
+
+
+const hundread = 1000000;
+console.log(hundread.toLocaleString('en-IN'));
