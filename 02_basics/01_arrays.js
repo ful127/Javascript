fbcda6e9dba4d6bd24bba492fdf5285c60bcadb7
Abhishek sharma <132250580+ful127@users.noreply.github.com>
2024-01-10 19:27:57 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,20 @@
+const myArray = [1 , 2, 5,6];
+//console.log(myArray);
+
+/*myArray.push(7);
+console.log(myArray);
+myArray.pop();
+console.log(myArray);
+myArray.unshift(9);
+
+myArray.unshift(9);
+console.log(myArray);
+myArray.shift();
+console.log(myArray);
+*/
+m1 = myArray.slice(1,3)
+console.log(m1);
+console.log(myArray);
+m2 = myArray.splice(1,3);
+console.log(m2);
+console.log(myArray);
